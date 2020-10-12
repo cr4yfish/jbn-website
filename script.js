3a7afa8b2bd5b0b83e726ceefb697cf978a25a68
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2020-10-12 21:16:49 +0200

    update

--- script.js
+++ script.js
@@ -4,15 +4,12 @@ function buttonTransitionFunction(x) {
     x.classList.toggle("change");
 }
 
-
 function toggleNavBar() {
-
-    if(isShown == false) {
+    if (isShown == false) {
         document.getElementById("navbar").style.height = "100px";
         isShown = true;
-    } else if(isShown == true) {
+    } else if (isShown == true) {
         document.getElementById("navbar").style.height = "0px";
         isShown = false;
     }
-
 }
