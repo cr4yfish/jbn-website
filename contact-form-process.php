02916908a937119498fb54262740ac6bcad2d306
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2021-04-10 01:34:09 +0200

    removed error message

--- contact-form-process.php
+++ contact-form-process.php
@@ -10,13 +10,3 @@ mail('cr4yfish1@gmail.com', 'message Form Submission', $message);
 
 
 ?>
-
-<html>
-<head>
-<title>Something went wrong</title>
-</head>
-<body>
-<h1>Something went wrong</h1>
-<p>We could not send your message. Please try again.</p>
-</body>
-</html>
