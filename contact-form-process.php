10cf400425f41009d42001ff9ee0ef109509d181
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2021-04-10 16:26:27 +0200

    fixed $user_name not appearing in email

--- contact-form-process.php
+++ contact-form-process.php
@@ -8,8 +8,9 @@ $user_name = $_POST['user_name'];
 $message = $_POST['message'];
 
 #Send email
-$headers = "From: $user_name";
-mail('cr4yfish1@gmail.com', 'message Form Submission', $message);
+$headers = "FROM_: $user_name.";
+$emailed_message = "$headers NACHRICHT_: $message";
+mail('cr4yfish1@gmail.com', 'message Form Submission', $emailed_message);
 
 $plswork = TRUE;
 
