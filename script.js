3f09e80222818460673a6dc73a9a0279a2868815
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2021-04-11 20:32:20 +0200

    added dark theme with working switch and localStorage

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+function checkTheme() {
+    // find out if darkTheme has already been initialized.
+    if ( localStorage.getItem("darkTheme") == "true" ) {
+        // darkTheme is in storage and set true, which means dark theme should be enabled.
+        console.log( "darkTheme has been set as true");
+        enableTheme(true);
+    } else {
+        // darkTheme is either false or has not changed yet, which means 
+        // the website should default to darkTheme = false.
+        
+        // store darkTheme = false in case it has not been stored yet.
+        // If it has, darkTheme will overwrite darkTheme == false, with darkTheme = false, which
+        // doesnt change anything.
+        localStorage.setItem("darkTheme", "false");
+        console.log("dark theme has not been set yet or is false");
+    }
+}
+
+function enableTheme(x) {
+    if (x == true) {
+        var divs = document.querySelectorAll('.dark_comp');
+        for (var i = 0; i < divs.length; i++) {
+            // add dark_toggle to enable dark Theme.
+            divs[i].classList.add('dark_toggle');
+        }
+        // store darkTheme = true value, since darkTheme is now enabled
+    }
+}
+
+// switch is clicked:
+function toggleTheme(x) {
+    if (localStorage.getItem("darkTheme" ) == "true") {
+        // darkTheme == true = darkTheme is on. User wants to disable darkTheme.
+        var divs = document.querySelectorAll('.dark_comp');
+        for (var i = 0; i < divs.length; i++) {
+            // remove dark_toggle from all dark_comp elements.
+            // Dark theme should be disabled now.
+            divs[i].classList.remove('dark_toggle');
+        }
+        // store darkTheme = false value, since darkTheme is now disabled
+        localStorage.setItem("darkTheme", "false");
+        console.log("dark theme has been disabled");
+    } else if (localStorage.getItem("darkTheme") == "false") {
+        // else == dark theme is false. User wants to enable it.
+        var divs = document.querySelectorAll('.dark_comp');
+        for (var i = 0; i < divs.length; i++) {
+            // add dark_toggle to enable dark Theme.
+            divs[i].classList.add('dark_toggle');
+        }
+        // store darkTheme = true value, since darkTheme is now enabled
+        localStorage.setItem("darkTheme", "true");
+        console.log("dark theme has been enabled");
+    }
+
+}
+
+
 function buttonTransitionFunction(x) {
     x.classList.toggle("change");
 }
@@ -19,3 +76,9 @@ function pop_up_trigger() {
     document.getElementById("email_confirm_popup").style.opacity = "1";
 }
 
+
+// Ways to implement a dark mode switch
+// 1. Add a dark_toggle class to all elements
+// 2. add a dark_comp class to elements that can switch to dark mode, 
+// then get those elements and toggle dark-toggle on them
+// 3. Google a more sensible solution
