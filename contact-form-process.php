f96e79f854c4c5ee73fc64369d3d6826c2ab8ce9
Cr4yfish <58395553+cr4yfish@users.noreply.github.com>
2021-04-15 21:18:49 +0200

    changed mail

--- contact-form-process.php
+++ contact-form-process.php
@@ -8,7 +8,7 @@ $message = $_POST['message'];
 #Send email
 $headers = "from: $user_name.";
 $emailed_message = "$message";
-mail("cr4yfish1@gmail.com", "Website: Neue Einreichung von: $headers", "$emailed_message");
+mail("cr4yfish.public@protonmail.com", "Website: Neue Einreichung von: $headers", "$emailed_message");
 
 header("Location: kontakt.html");
 exit();
